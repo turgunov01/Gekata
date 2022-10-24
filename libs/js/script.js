49b2cfb72ef9c9e2684cb2c0160d4f6ed1121da5
Ваше Имя <you@example.com>
2022-10-24 09:52:32 +0500

    redone

---
+++ libs/js/script.js
@@ -0,0 +1,30 @@
+const navList = document.querySelector('.header__list')
+let lines = document.querySelector('.lines')
+let line1 = document.querySelector('.header__lines::after')
+let line2 = document.querySelector('.header__lines::before')
+
+
+
+lines.addEventListener('click', () => {
+    lines.classList.toggle('active')
+    navList.style.transition = '300ms'
+
+    if (lines.classList.contains('active')) {
+        navList.style.top = '130px'
+    } else {
+        navList.style.top = '-100%'
+    }
+})
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        speed: 1000,
+        transition: 1000,
+    }
+});
